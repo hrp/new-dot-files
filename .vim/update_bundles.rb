e8ffa9d8936ce17b2bf38b760237694585451f19
hrp <hrparmar@gmail.com>
2010-08-16 17:11:49 -0700

    installed vim plugins

---
+++ .vim/update_bundles.rb
@@ -0,0 +1,59 @@
+#!/usr/bin/env ruby
+# Automated download and update of vim plugins
+# http://tammersaleh.com/posts/the-modern-vim-config-with-pathogen
+
+git_bundles = [ 
+  "git://github.com/scrooloose/nerdtree.git",
+  "git://github.com/tpope/vim-haml.git",
+  "git://github.com/tpope/vim-markdown.git",
+  "git://github.com/tpope/vim-repeat.git",
+  "git://github.com/tpope/vim-surround.git",
+  "git://github.com/tpope/vim-vividchalk.git",
+  "git://github.com/tsaleh/vim-align.git",
+  "git://github.com/tsaleh/vim-shoulda.git",
+  "git://github.com/tsaleh/vim-supertab.git",
+  "git://github.com/tsaleh/vim-tcomment.git",
+  "git://github.com/vim-ruby/vim-ruby.git",
+  "git://repo.or.cz/vcscommand",
+  "git://github.com/vim-bundles/fuzzyfinder.git",
+  "git://github.com/borgand/ir_black.git"
+]
+
+vim_org_scripts = [
+  ["IndexedSearch",      "7062",  "plugin"],
+  ["gist",               "12732", "plugin"],
+  ["jquery",             "12107", "syntax"],
+  ["taglist", "7701",    "zip"]
+]
+
+require 'fileutils'
+require 'open-uri'
+
+bundles_dir = File.join(File.dirname(__FILE__), "bundle")
+
+FileUtils.cd(bundles_dir)
+
+puts "Trashing everything (lookout!)"
+Dir["*"].each {|d| FileUtils.rm_rf d }
+
+git_bundles.each do |url|
+  dir = url.split('/').last.sub(/\.git$/, '')
+  puts "  Unpacking #{url} into #{dir}"
+  `git clone #{url} #{dir}`
+  FileUtils.rm_rf(File.join(dir, ".git"))
+end
+
+vim_org_scripts.each do |name, script_id, script_type|
+  puts "  Downloading #{name}"
+  local_file = File.join(name, script_type, "#{name}.#{script_type == 'zip' ? 'zip' : 'vim'}")
+  FileUtils.mkdir_p(File.dirname(local_file))
+  File.open(local_file, "w") do |file|
+    file << open("http://www.vim.org/scripts/download_script.php?src_id=#{script_id}").read
+  end
+  if script_type == 'zip'
+    %x(unzip -d #{name} #{local_file})
+  end
+end
+
+# Regenerate the helpfiles
+`vim -e -c .call pathogen#helptags(). -c q`
